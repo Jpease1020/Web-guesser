f840fa333bd10767a39eb511985437cb0eaacb1e
Justin Pease <justinpease2@gmail.com>
2015-08-22 11:58:40 -0600

    some features addded

--- web_guesser.rb
+++ web_guesser.rb
@@ -11,51 +11,45 @@ get '/' do
 end
 
 def check_guess(user_guess, number)
+  if @@counter == 0
+    restart
+  else
+    guess_actions(user_guess, number)
+  end
+end
+
+def guess_actions(user_guess, number)
   @@counter -= 1
-   if user_guess == 0 && @@counter > 0
-     if @@counter == 0
-       restart
-     else
+   if user_guess == 0
        @color = '#FFFFFF'
-       "Take a Guess!"
-     end
-   elsif user_guess > number + 5 && @@counter > 0
-     if @@counter == 0
-       restart
-     else
+       "You have #{@@counter} guesses left. Take a Guess!"
+   elsif user_guess > number + 5
        @color = '#FF0000'
-       "Way too high!"
-     end
-   elsif user_guess > number && @@counter > 0
-     if @@counter == 0
-       restart
-     else
+       "Way too high!  You have #{@@counter} guesses left "
+   elsif user_guess > number
        @color = "#FF9090"
-      "Too high"
-     end
-   elsif user_guess < number - 5 && @@counter > 0
-     if @@counter == 0
-       restart
-     else
+      "Too high. You have #{@@counter} guesses left"
+   elsif user_guess < number - 5
        @color = '#FF0000'
-       "Way too low!"
-     end
-   elsif user_guess < number && @@counter > 0
-     if @@counter == 0
-       restart
-     else
+       "Way too low! You have #{@@counter} guesses left"
+   elsif user_guess < number
        @color = "#FF9090"
-       "Too low!"
-     end
-   elsif user_guess == number && @@counter > 0
+       "Too low. You have #{@@counter} guesses left"
+   elsif user_guess == number
       @color = "#00FF00"
-      "The SECRET NUMBER is #{number} You got it right!"
+      new_game
+      "The SECRET NUMBER is #{number} You got it right! A new game has begun, have fun playing! "
    end
+end
+
+def new_game
+  @@counter = 5
+  NUMBER
 end
 
 def restart
   @color = '#FFFFFF'
   @@counter = 5
-  Number
+  NUMBER
   "You've lost, a new number will be generated and the game will start over."
 end
